d0f7f0761b0efd564e3d63f0100143ac78b9f98d
Jenn66 <1243449096@qq.com>
2019-12-06 15:41:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/test3/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.test3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1=(Button)findViewById(R.id.ButtonAnother);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction("startAnotherActivity");
+                // intent.addCategory("android.intent.category.DEFAULT");
+                startActivity(intent);
+            }
+        });
+
+        
+
+    }
+}
